bc7eb13f0321ee8c7860e1dda2f34dc11e8d04c8
Mohammed El Mangadi <138013464+Riyadaram@users.noreply.github.com>
2024-12-08 23:37:58 +0000

    Second Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,52 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const palos = ["heart", "diamond", "spade", "club"];
+const valores = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generarCarta() {
+  const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+
+  const cardContainer = document.querySelector("#card-container .card");
+
+  cardContainer.classList.remove(...palos);
+  cardContainer.classList.add(paloAleatorio);
+
+  cardContainer.innerHTML = `${valorAleatorio} ${obtenerSimbolo(
+    paloAleatorio
+  )}`;
+}
+
+function obtenerSimbolo(palo) {
+  switch (palo) {
+    case "heart":
+      return "♥";
+    case "diamond":
+      return "♦";
+    case "spade":
+      return "♠";
+    case "club":
+      return "♣";
+  }
+}
+
+document
+  .getElementById("new-card-button")
+  .addEventListener("click", generarCarta);
+
+window.onload = generarCarta;
+
+setInterval(generarCarta, 10000);
