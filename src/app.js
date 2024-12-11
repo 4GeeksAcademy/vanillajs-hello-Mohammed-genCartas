2b3c7f5a2bd2cafef76105a8b88904dbbf2fadfb
Mohammed El Mangadi <138013464+Riyadaram@users.noreply.github.com>
2024-12-11 16:55:10 +0000

    Commit

--- src/app.js
+++ src/app.js
@@ -22,27 +22,31 @@ let cardNumber = [
 let palos = ["♥", "♦", "♠", "♣"];
 
 window.onload = function() {
-  let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
-  let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
-  let randomSuitNumber = Math.floor(Math.random() * suit.length);
-  let finalSuit = suit[randomSuitNumber];
+  const generateCard = document.getElementById("new-card-button");
+  generateCard.addEventListener("click", randomCard);
+  function randomCard() {
+    let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+    let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+    let randomSuitNumber = Math.floor(Math.random() * suit.length);
+    let finalSuit = suit[randomSuitNumber];
 
-  document.getElementById("cardContent").innerHTML =
-    cardNumber[randomCardNumber];
-  document.getElementById("theCard").className = "";
-  document.getElementById("theCard").classList.add("card");
-  document.getElementById("theCard").classList.add(getSuiteClass(finalSuit));
-};
-
-function getSuiteClass(suit) {
-  switch (suit) {
-    case "Diamonds":
-      return "suit-diamonds";
-    case "Spades":
-      return "suit-spades";
-    case "Hearts":
-      return "suit-hearts";
-    case "Clubs":
-      return "suit-clubs";
+    document.getElementById("cardContent").innerHTML =
+      cardNumber[randomCardNumber];
+    document.getElementById("theCard").className = "";
+    document.getElementById("theCard").classList.add("card");
+    document.getElementById("theCard").classList.add(getSuiteClass(finalSuit));
+    function getSuiteClass(suit) {
+      switch (suit) {
+        case "Diamonds":
+          return "suit-diamonds";
+        case "Spades":
+          return "suit-spades";
+        case "Hearts":
+          return "suit-hearts";
+        case "Clubs":
+          return "suit-clubs";
+      }
+    }
   }
-}
+  randomCard();
+};
